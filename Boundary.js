0d7ceb5c3e00a62cebf1b1b50bb8c66a8990c613
Benjamin M. Brown <benjaminbb@gmail.com>
2016-08-05 12:50:25 -0500

    Adding box2d References

--- Boundary.js
+++ Boundary.js
@@ -1,4 +1,4 @@
-var Boundary = function(){
+var Boundary = function(x_,y_,w_,h_){
 	/*
 	5 STEPS
 	1. Define the body using BodyDef
@@ -7,5 +7,35 @@ var Boundary = function(){
 	4. Define fixture using fixtureDef
 	5. Attach shape to body
 	*/
+
+	this.x = x_;
+	this.y = y_;
+	this.w = w_;
+	this.h = h_;
+
+	var fixtureDef = new box2d.b2FixtureDef();
+	fixtureDef.density = 1.0;
+	fixtureDef.friction = 0.5;
+	fixtureDef.restitution = 0.2;
+
+	var body = new box2d.b2BodyDef();
+	body.type = box2d.b2BodyType.b2_staticBody;
+	body.position.x = scaleToWorld(this.x);
+	body.position.y = scaleToWorld(this.y);
+
+	fixtureDef.shape = new box2d.b2PolygonShape();
+	fixtureDef.shape.SetAsBox(this.w/(scaleFactor*2),this.h/(scaleFactor*2));
+	this.body = world.CreateBody(body).CreateFixture(fixtureDef);
+
+
+
+	this.display = function(){
+		fill(127);
+		stroke(0);
+		rectMode(CENTER);
+		rect(this.x,this.y, this.w,this.h);
+	}
+
+
 	
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,47 @@
-var box2d;
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+    b2BodyDef = Box2D.Dynamics.b2BodyDef,
+    b2Body = Box2D.Dynamics.b2Body,
+    b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+    b2Fixture = Box2D.Dynamics.b2Fixture,
+    b2World = Box2D.Dynamics.b2World,
+    b2MassData = Box2D.Collision.Shapes.b2MassData,
+    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+    b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+
+var world;
+var boundaries = [];
+var boxes = [];
+
 
 
 function setup() {
+	createCanvas(640,360);
+	//var world = new box2d.b2World();
+	world = new b2World(new b2Vec2(0, 10),true);
+	//var world = new b2World( new b2Vec2(0.0, -10.0) );
+	//var world = createWorld();
+	var b = new Box(width/2,30);
 
 }
 
 function draw(){
+	background(51);
+	var timeStep = 1.0/30;
+	world.step(timeStep,10,10);//2&3 locs are for velocity and pos iterators
+
+	//boxes get randomly generated
+	if(random(1)<0.2){
+		var b = new Box(width/2,30);
+		boxes.push(b);
+	}
+
+	for(var i = boxex.length-1; i>=0; i--){
+		boxes[i].display();
+		if(boxes[i].done()){
+			boxes.splice(i,1);
+		}
+	}
 
 }
 
